874229d9de8462a21abef2e70fb7e25af01f9d97
odi09 <60698046+odi09@users.noreply.github.com>
2020-08-20 20:32:05 -0300

    Add files via upload

--- RECI2020/2020_2Cuatri_Recu_fuentes(noche)/dos.js
+++ RECI2020/2020_2Cuatri_Recu_fuentes(noche)/dos.js
@@ -5,7 +5,67 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más liviano de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+*/function mostrar() {
+  let marca;
+  let precio;
+  let peso;
+  let tipo;
+  let respuesta = "s";
+  let acumuPeso = 0;
+  let maxMarca;
+  let precioMax;
+  let bandera = 0;
+  let banderaDos = 0;
+  let pesoMin;
+  let marcaMin;
+  do {
+    marca = prompt("Ingrese marca");
+
+    precio = prompt("Ingrese precio");
+    precio = parseInt(precio);
+    while (precio < 1 || isNaN(precio) == true) {
+      precio = prompt("Ingrese un precio VALIDO");
+      precio = parseInt(precio);
+    }
+
+    peso = prompt("Ingrese peso");
+    peso = parseInt(peso);
+    while (peso < 1 || isNaN(peso) == true) {
+      peso = prompt("Ingrese un peso VALIDO");
+      peso = parseInt(peso);
+    }
+
+    tipo = prompt("Ingrese tipo: sólido o líquido");
+    while (tipo != "sólido" && tipo != "solido" && tipo != "líquido" && tipo != "liquido") {
+      tipo = prompt("Ingrese un tipo VALIDO: sólido o líquido");
+    }
+
+    acumuPeso = acumuPeso + peso;
+    switch (tipo) {
+      case "líquido":
+      case "liquido":
+        if (bandera == 0 || precioMax < precio) {
+          precioMax = precio;
+          maxMarca = marca;
+          bandera = 1;
+        }
+        break;
+      case "sólido":
+      case "solido":
+        if (banderaDos == 0 || pesoMin > peso) {
+          pesoMin = peso;
+          marcaMin = marca;
+          banderaDos = 1;
+        }
+        break;
+    }
+    respuesta = prompt("Desea ingresar otro datos: s/n");
+  } while (respuesta == "s");
+
+  //a)informar el peso total de la compra.
+  document.write("El peso total es " + acumuPeso + "<br>");
+  //b)la marca del más caro de los líquidos
+  document.write("La marca mas cara de los liquidos es " + maxMarca + "<br>");
+  //c)la marca del más liviano de los sólidos
+  document.write(" La marca del más liviano de los sólidos " + marcaMin + "<br>");
 }
--- RECI2020/2020_2Cuatri_Recu_fuentes(noche)/tres.js
+++ RECI2020/2020_2Cuatri_Recu_fuentes(noche)/tres.js
@@ -9,7 +9,84 @@ a)el lugar más elegido
 b)el nombre de titular que lleva más pasajeros.
 c)el promedio de personas por viaje,  que viajan en invierno
 */
-function mostrar()
-{
-	alert("tres");
+function mostrar() {
+	let respuesta = "s";
+	let nombre;
+	let lugar;
+	let temporada;
+	let cantidad;
+	let contaBali = 0;
+	let contaCata = 0;
+	let contaSal = 0;
+	let bandera = 0;
+	let cantiMax;
+	let nombreMax;
+	let contaPersonas = 0;
+	let acumuCanti = 0;
+	let promedioPersonas;
+
+	do {
+		nombre = prompt("Ingrese nombre");
+
+		lugar = prompt("Ingrese lugar: bariloche, cataratas o salta");
+		while (lugar != "bariloche" && lugar != "cataratas" && lugar != "salta") {
+			lugar = prompt("Ingrese lugar VALIDO: bariloche, cataratas o salta");
+		}
+
+		temporada = prompt("Ingrese temporada: otoño, invierno, verano, primavera");
+		while (temporada != "otoño" && temporada != "invierno" && temporada != "verano" && temporada != "primavera") {
+			temporada = prompt("Ingrese temporada VALIDA: otoño, invierno, verano, primavera");
+		}
+
+		cantidad = prompt("Ingrese cantidad");
+		cantidad = parseInt(cantidad);
+		while (cantidad < 1 || isNaN(cantidad) == true) {
+			cantidad = prompt("Ingrese cantidad VALIDAD");
+			cantidad = parseInt(cantidad);
+		}
+
+		switch (lugar) {
+			case "bariloche":
+				contaBali++;
+				break;
+			case "cataratas":
+				contaCata++;
+				break;
+			case "salta":
+				contaSal++;
+				break;
+		}
+
+		if (bandera == 0 || cantiMax < cantidad) {
+			cantiMax = cantidad;
+			nombreMax = nombre;
+		}
+
+		switch (temporada) {
+			case "invierno":
+				acumuCanti = acumuCanti + cantidad;
+				break;
+		}
+		contaPersonas++;
+
+		respuesta = prompt("Desea ingresar mas datos? s/n");
+	} while (respuesta == "s");
+
+	//a) el lugar más elegido
+	if (contaBali > contaCata && contaBali > contaSal) {
+		document.write("El lugar mas elegido es Bariloche" + "<br>");
+	}
+	else if (contaCata > contaBali && contaCata > contaSal) {
+		document.write("El lugar mas elegido es Cataratas" + "<br>");
+	}
+	else if (contaSal > contaBali && contaSal > contaCata) {
+		document.write("El lugar mas elegido es Salta" + "<br>");
+	}
+	//b) el nombre de titular que lleva más pasajeros.
+	document.write("El nombre de titular que lleva más pasajeros " + nombreMax + "<br>");
+
+	//c) el promedio de personas por viaje, que viajan en invierno
+	promedioPersonas = acumuCanti / contaPersonas;
+	document.write("El promedio de personas por viaje, que viajan en invierno " + promedioPersonas + "<br>");
+
 }
